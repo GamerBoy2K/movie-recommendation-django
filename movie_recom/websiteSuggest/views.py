dc3d61a8ba092e31881b078f847fe636879c1720
Sounak Pal <sounakpal1@gmail.com>
2023-04-16 12:44:25 +0530

    full setup with a demo index page

--- movie_recom/websiteSuggest/views.py
+++ movie_recom/websiteSuggest/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
+def indexPage(request):
+    return render(request,'index.html')
 # Create your views here.
