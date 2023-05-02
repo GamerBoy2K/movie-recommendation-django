a1023ae03fae6e82f0ca54ada99ba68ff1527e5e
Sounak Pal <sounakpal1@gmail.com>
2023-05-02 16:39:09 +0530

    images to model and media to settings

--- movie_recom/websiteSuggest/models.py
+++ movie_recom/websiteSuggest/models.py
@@ -7,5 +7,6 @@ class movies(models.Model):
     movieId=models.IntegerField(primary_key=True)
     title=models.CharField(max_length=250)
     genres=models.CharField(max_length=250)
+    imagePoster = models.ImageField(upload_to='images')  
     class Meta:
         db_table="movies"
