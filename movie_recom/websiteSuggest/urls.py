59b46ffc218c985edc2abb5dde7ff43a49d02657
Sounak Pal <sounakpal1@gmail.com>
2023-05-21 18:17:48 +0530

    login error fixed

--- movie_recom/websiteSuggest/urls.py
+++ movie_recom/websiteSuggest/urls.py
@@ -10,14 +10,14 @@ urlpatterns=[
     path('logout',views.logout_users),
     path('signup',views.register_user),
     path('searchResult',views.searchResult),
-    path('search',views.search),
+    path('search',views.search), #debug
     path('movieDetails/<int:id>',views.movieDetails),
-    path('movies/<int:pno>',views.allMovies),
-    path('addMovies',views.addMovies),
-    path('addMov',views.addMov),
-    path('edit/<int:id>',views.editMovie),
-    path('editMovFinal',views.editFinalMovie),
-    path('invalidCredentials',views.invalidCredentials),
-    path('adminDashboard',views.adminDashboard),
+    path('movies/<int:pno>',views.allMovies),#Need Edit
+    path('addMovies',views.addMovies), #part 1 add
+    path('addMov',views.addMov), #part 2 add
+    path('edit/<int:id>',views.editMovie), #part 1 edit
+    path('editMovFinal',views.editFinalMovie), ##part 2 edit
+    path('invalidCredentials',views.invalidCredentials), #debug
+    path('adminDashboard',views.adminDashboard), #need Edit
    
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
